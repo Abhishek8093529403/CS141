03b5a8e603a17d8d30589bbd2b54804eb3eebd26
Abhishek8093529403 <42151525+Abhishek8093529403@users.noreply.github.com>
2018-08-16 14:22:36 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,23 @@
+//include the necessary library
+#include<iostream>
+using namespace std;
+//declarimg the main function
+int main()
+{
+    int value ;
+    // declaring variables of all types
+int a,b;
+    float floatvar = 0.25;
+double doublevar = 1.2;
+     bool boolvar = true;
+char character = 'C';
+
+//displaying the sizes of data types.
+cout << "size of a = " << sizeof(a) << endl;
+cout << "size of doublevar = " << sizeof(doublevar) << endl;
+cout << "size of floatvar = " << sizeof(floatvar) << endl;
+cout << "size of boolvar = " << sizeof(boolvar) << endl;
+cout << "size of character = " << sizeof(character) << endl;
+
+}
+
