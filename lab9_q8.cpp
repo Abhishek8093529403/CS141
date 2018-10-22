f7408e102ad21517296915ad617b7e6bb04fb189
Abhishek8093529403 <42151525+Abhishek8093529403@users.noreply.github.com>
2018-10-22 23:12:37 +0530

    Update lab9_q8.cpp

--- lab9_q8.cpp
+++ lab9_q8.cpp
@@ -1,5 +1,8 @@
+//Write a function myStrLen(char*) which returns the length of the parameter cstring. 
+//Write the function without using the C++ function strlen.
 #include <iostream>
 using namespace std;
+//using the function
 int myStrLen(char* p)
 {
     int count=0;
@@ -14,7 +17,7 @@ int main()
 {
     char ar[100];
     cout<<"Enter the string:"<<ar;
-    cin.getline(ar,100);
+    cin.getline(ar,100);//getting a string line
     char* p=&ar[0];
     cout<<"Length of the string:"<<myStrLen(p);
     return 0;
