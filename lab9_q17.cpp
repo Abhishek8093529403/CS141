a81b7001d6f684ee5eaac82fa58cf91ba129b88f
Abhishek8093529403 <42151525+Abhishek8093529403@users.noreply.github.com>
2018-10-27 11:58:07 +0000

    Update lab9_q17.cpp

--- lab9_q17.cpp
+++ lab9_q17.cpp
@@ -7,118 +7,204 @@ Strchr
 Strstr  */
 
 #include <iostream>
-#include<cstring>
 using namespace std;
+
+char* copy(char* str2,char* str)
+{
+	if (str==NULL)
+		return NULL;
+	char* ptr=str2;
+	while(*str!='\0')
+	{
+		*str2=*str;
+		str2++;
+		str++;
+	}
+	*str2='\0';
+	return ptr;
+}
+
+
+
+
+char* cat(char* str,char* str3)
+{
+	int size1=0;//size2=0;
+	char* z=str;
+	while(*z!='\0')
+	{
+		size1++;
+		z++;
+	}
+	char* p=str+size1;
+	while(*str3!='\0')
+	{
+		*p++=*str3++;
+	}
+	*p='\0';
+	return str;
+}
+
+
+
+int len(char* str)
+{
+	int size=0;
+	while(*str!='\0')
+	{
+		size++;
+		str++;
+	}
+	return size;
+}
+
+
+
+int compare(char* a,char*b)
+{
+int result=0;
+	for (int i = 0;*(a+i)!='\0' || *(b+i)!='\0' ; i++)
+	{
+		if (*(a+i) != *(b+i))
+		{
+		    result= *(a+i)-*(b+i);
+			break;
+		}
+	}
+return result;
+
+}
+
+
+
+char* ckch(char* str,char c)
+{
+	char* p=str;
+	if(str==NULL)
+	return NULL;
+	
+	else
+	{
+		for(int i=0;*(p+i)!='\0';i++)
+		{
+			if(*(p+i)==c)
+			{
+				return p+i;
+			}
+		}
+	}
+}
+
+
+
+// returns true if strings are same
+int comp(const char *str1, const char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+			return 0;
+		str1++;
+		str2++;
+	}
+
+	return (*str2=='\0');
+}
+// Function to implement strstr() function
+const char* strstr(const char* str1,const char* str2)
+{
+	while(*str1!='\0')
+	{
+		if(*str1==*str2 && comp(str1,str2))
+			return str1;
+		str1++;
+	}
+}
+
+
+
+
 int main()
 {
-    //Declaring intial string
+     //Declaring intial string
     char str[20];
-    cout<<"Enter string 1"<<endl;
-    cin>>str;
-    
-    cout<<endl;
-    
-    //Implementation of strcpy function
-    char str2[20];
-    strcpy(str2,str);//Copying value of str to str2
-    cout<<"Output of strcpy function"<<endl;
-    cout<<str2<<endl;//Print output
-    
-    cout<<endl;
-    
-    //Implementation of strlen function
-    cout<<"Use of strlen function"<<endl;
-    cout<<"Lenght of str:"<<strlen(str)<<endl;//Shows the length of str
-    cout<<"Lenght of str2:"<<strlen(str2)<<endl;//Show the length of str2
-    
-    cout<<endl;
-    
-    //Implementation of strcmp function
-    char str3[20];
-    cout<<"Enter string 3"<<endl;
-    cin>>str3;
-    
-    cout<<endl;
-    
-    cout<<"Use of strcmp function"<<endl;
-    int result=strcmp(str,str3);//Compares the lengthof two strings
-    if(result==0)
-    cout<<"Strings are same or identical"<<endl;
-    else if(result>0)
-    cout<<"The ascii value of the character of str after 1st mismatch is more than that of str3"<<endl;
-    else
-    cout<<"The ascii value of the character of str after 1st mismatch is less than that of str3"<<endl;
-    
-    cout<<endl;
-    
-    //Implementation of strcat function
-    cout<<"Output of strcat function"<<endl;
-    strcat(str,str3);//Concatenating str with str3
-    cout<<str<<endl;//output represented by the string whcih is concatenated by other string
-    
-    cout<<endl;
-    
-    //Implementation of strchr function
-    cout<<"Use of strchr function"<<endl;
-    char str1[20];
     cout<<"Enter string"<<endl;
-    cin>>str1;
-    
+    cin.getline(str,20);
     cout<<endl;
+ //Implementation of strcpy function
+	cout<<"Use of strcpy"<<endl;
+    char str2[20];
+    cout<<"Output of str2 which is copy of the string given:"<<endl<<copy(str2,str)<<endl<<endl;
+
+
+   
+    //Implementation of strcat function
+	char str3[20];
+	cout<<"Enter other string"<<endl;
+	cin.getline(str3,20);
+    cout<<"Output after strcat function:"<<cat(str,str3)<<endl<<endl;
+
+
+
+
+   //Implementation of strlen function
+    cout<<"Use of strlen function"<<endl;
+	char* p=str;
+    cout<<"Length of str(after concatenation):"<<len(p)<<endl<<endl;
+ 
+
+
+   
+    //Implementation of strcmp function
+    cout<<"Use of strcmp function"<<endl;
+	char str4[20];
+	cout<<"Enter other string"<<endl;
+	cin.getline(str4,20);
+    int result=compare(str,str4);//Compares the lengthof two strings
+    if(result==0)
+    cout<<"Strings are same"<<endl<<endl;
+    else if(result>0)
+    cout<<"The ascii value of the character of str after 1st mismatch is more than that of str2 with ascii difference:"<<result<<endl<<endl;
+    else
+    cout<<"The ascii value of the character of str after 1st mismatch is less than that of str2 with ascii difference:"<<result<<endl<<endl;
+
+
+
     
+  //Implementation of strchr function
+   cout<<"Use of strchr function"<<endl;
     char c;
     cout<<"Enter the character you want to find in the string"<<endl;
     cin>>c;
-    
-    cout<<endl;
-    
-    char* ch = strchr(str1, c); 
-    if(ch!=NULL)//Check whether character is present in the string or not
-    {
-        cout<<"Character is present in the string"<<endl;
-        while (ch!=NULL)//Loop to find out the postion at which the Character is occuring in the string
-        {
-            cout<<"found at:"<<(ch-str1+1)<<endl;
-            ch=strchr(ch+1,c);
-        }
-    }
-    else
-    {
-        cout<<"Character is absent in the string"<<endl;
-    }
-    
-    cout<<endl;
-    
-    //Implementation of strstr function
+char* p1=str;
+if(ckch(p1,c)==NULL)
+{
+cout<<"The character is absent in the string"<<endl;
+}
+else{
+while(ckch(p1,c)!=NULL)
+{
+	cout<<"Found at:"<<(ckch(p1,c)-str+1)<<endl;
+	p1=ckch(p1,c)+1;
+}
+}
+cout<<endl;
+
+
+
+   //Implementation of strstr function
     cout<<"Use of strstr function"<<endl;
-    char str4[20];
-    cout<<"Enter str4"<<endl;
-    cin>>str4;
-    
-    cout<<endl;
-    
-    cout<<"Str4="<<str4<<endl;
-    
-    cout<<endl;
-    
-    char str5[20];
-    cout<<"Enter str5"<<endl;
-    cin>>str5;
-    
-    cout<<endl;
-    
+	cin.ignore();
+    char str5[50];
+    cout<<"Enter str0"<<endl;
+    cin.getline(str5,50);
     cout<<"Str5="<<str5<<endl;
-    
-    cout<<endl;
-    
-    char* p=strstr(str4,str5);//Check the first occurance of str4 in str3
-    if(p)
-    {
-        strcpy(p,str5);
-        cout<<"Str5 is present in str4"<<endl;
-    }
-    else
-    cout<<"Str5 is not present in str4"<<endl;
-    cout<<str4<<endl;
+    char str6[50];
+    cout<<"Enter str6"<<endl;
+    cin.getline(str6,50);
+    cout<<"Str6="<<str6<<endl;
+	cout<<strstr(str5,str6)<<endl;
+
+
     return 0;
 }
